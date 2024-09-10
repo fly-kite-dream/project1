3e6320336fe93cb2e92d678f2caca08f25ffa817
fly-kite-dream <cheng122688@163.com>
2024-09-10 12:51:31 +0800

    Add files via upload

---
+++ 回文数.cpp
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("请输入一个整数来判段是否为回文数");
+//	scanf_s("%d", &a);
+//	int A = a;//保留原始值
+//	int c = 0;
+//	while(a!=0)//这一段循环是重点！！！！！！
+//	{
+//		int b = a % 10;
+//		c = c * 10 + b;
+//		a = a / 10;
+//	}
+//	if(c==A)
+//	{
+//		printf("YES");
+//	}
+//	else if(c!=A)
+//	{
+//		printf("NO");
+//	}
+//	return 0;
+//}
