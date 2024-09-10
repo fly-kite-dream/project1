4ca3894f3ddd3cd6e9d7b74d561812cddf4fa798
fly-kite-dream <cheng122688@163.com>
2024-09-10 17:29:29 +0800

    Add files via upload

---
+++ 去除前导零.cpp
@@ -0,0 +1,49 @@
+//#include <iostream>
+//using namespace std;
+//#pragma warning(disable:4996)
+//int main()
+//{
+//	int a;//判断运行数量
+//	scanf("%d", &a);
+//	for (int i = 0; i < a; i++)
+//	{
+//		char goal[1000];//用于输入的字符串
+//		scanf("%s", goal);
+//		char d[1000];//用于替换新的字符串
+//		int num = 0;//新字符串的索引
+//		int tool = 0;
+//		
+//		
+//		
+//			for (int b = 0; b < 5; b++)
+//			{
+//				if (goal[4] == '0')
+//				{
+//					printf("0");
+//					
+//					break;
+//				}
+//
+//
+//				else if (goal[b] != '0')
+//				{
+//					d[num] = goal[b];
+//					num++;
+//
+//
+//				}
+//
+//
+//
+//			}
+//			d[num] = '\0';//这一步很重要，将num与索引巧妙结合起来
+//			printf("%s\n", d);
+//		
+//		
+//	}
+//
+//	return 0;
+//
+//}
+//
+
