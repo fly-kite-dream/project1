d83d3a4b3f51bf2cc81016f246e2fa228f9ddfee
张成 <cheng122688@163.com>
2024-10-20 18:36:17 +0800

    Add files via upload

---
+++ 2023工大杯 - D - 特殊加法.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#pragma warning(disable:4996)
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int* A = (int*)calloc(n, sizeof(int));
+	int goal = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int num; scanf("%d", &num); A[i] = num;
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		int Index = 0;
+		for (int a = n + 1 - i; a > 0; a--)
+		{
+			if (Index > n-i)break;
+			else {
+				int* arr = (int*)calloc(i, sizeof(int));//长度
+				for (int index = 0; index < i; index++)
+				{
+
+					arr[index] = A[Index+index];
+					
+
+				}
+				Index++;
+				std::sort(arr, arr + i);
+				goal = goal + arr[i - 2];//累加次小值
+			}
+		}
+	}
+	std::cout << goal;
+}
